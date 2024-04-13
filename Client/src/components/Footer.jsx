1555f08733a922153a5b1be46b66fa098f9b2ab1
Sarveshreg <sarveshregmi1@gmail.com>
2024-04-13 11:28:05 -0500

    footer

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='footer'>I am the footer</div>
+    <div className='footer'><img className='micro_image' src="https://pngimg.com/d/facebook_logos_PNG19750.png" alt="" height={50} width={50} /></div>
   )
 }
 
